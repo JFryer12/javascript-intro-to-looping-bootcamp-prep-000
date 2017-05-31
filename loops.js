08afe957570439549e53587787697e6706f8bc35
Jarel Fryer <jcf0023@auburn.edu>
2017-05-31 15:02:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -6,4 +6,25 @@ function forLoop(arr) {
       arr.push("I am ${i} strange loop.")
     }
   }
+  return arr
+}
+
+function whileLoop(num) {
+  var count = num
+  while (count > 0) {
+    console.log(count);
+    count--;
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(arr) {
+  do {
+    arr.pop();
+  } while (arr.length > 0 && maybeTrue());
+  return arr
 }
